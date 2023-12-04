28e45c4f15f0be169c43f03ea4ab1f46d4e28170
xcalibur91 <129805277+xcalibur91@users.noreply.github.com>
2023-12-04 14:22:44 -0400

    Update posts.js

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -115,8 +115,9 @@ export function fetchPosts() {
 
     const apiURL = process.env.AWS_API_URL;
     const SAVE_USER_API_BASE_URL = `https://${apiURL}.execute-api.us-east-1.amazonaws.com/termAssignmentStage`;
-    console.log("API URL from CLoud Formation apiURL:",SAVE_USER_API_BASE_URL);
+    console.log("API URL from CLoud Formation apiURL:",apiURL);
     console.log("SAVE_USER_API_BASE_URL:",SAVE_USER_API_BASE_URL);
+    console.log("All environment variables:", process.env);
     const apiGatewayEndpoint = `${SAVE_USER_API_BASE_URL}/fetchpost`;
 
     return axios.get(apiGatewayEndpoint)
